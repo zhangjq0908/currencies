df753366a447cf830c6468cc2e6b539f71d7ba53
chenchen6677 <zhangjq0908@gmail.com>
2023-12-09 01:03:05 +0800

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,24 +4,24 @@ plugins {
     id("com.android.application") version "8.2.0" apply false
     id("org.jetbrains.kotlin.android") version "1.9.21" apply false
     // dependency-update-checker
-  //  id("com.github.ben-manes.versions") version "0.50.0"
+    id("com.github.ben-manes.versions") version "0.50.0"
 }
 
 // only check for stable versions
-//tasks.withType<DependencyUpdatesTask> {
-  //  rejectVersionIf {
-    //    isNonStable(candidate.version) && !isNonStable(currentVersion)
-   // }
-// }
+tasks.withType<DependencyUpdatesTask> {
+    rejectVersionIf {
+        isNonStable(candidate.version) && !isNonStable(currentVersion)
+    }
+}
 
-/**
+
 fun isNonStable(version: String): Boolean {
     val stableKeyword = listOf("RELEASE", "FINAL", "GA").any { version.uppercase().contains(it) }
     val regex = "^[0-9,.v-]+(-r)?$".toRegex()
     val isStable = stableKeyword || regex.matches(version)
     return isStable.not()
 }
-**/
+
 allprojects {
     repositories {
         google()
