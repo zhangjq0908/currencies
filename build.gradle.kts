78f44e5572282d560b33e5ad819a218b2a896495
chenchen6677 <zhangjq0908@gmail.com>
2023-12-09 00:43:20 +0800

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,3 @@
-/**
 import com.github.benmanes.gradle.versions.updates.DependencyUpdatesTask
 
 plugins {
@@ -15,13 +14,14 @@ tasks.withType<DependencyUpdatesTask> {
     }
 }
 
+/**
 fun isNonStable(version: String): Boolean {
     val stableKeyword = listOf("RELEASE", "FINAL", "GA").any { version.uppercase().contains(it) }
     val regex = "^[0-9,.v-]+(-r)?$".toRegex()
     val isStable = stableKeyword || regex.matches(version)
     return isStable.not()
 }
-
+**/
 allprojects {
     repositories {
         google()
